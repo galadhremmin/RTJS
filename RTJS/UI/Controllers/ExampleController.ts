4b7fc8b71007eca98f4b9aaa533f228c27626428
galadhremmin <zanathel@gmail.com>
2014-10-08 14:23:22 +0200

    - Updated the example code with actions

--- RTJS/UI/Controllers/ExampleController.ts
+++ RTJS/UI/Controllers/ExampleController.ts
@@ -5,9 +5,12 @@ import IModel = require("../../Model/Abstract/IModel");
 export class ExampleController extends Controller<views.View, IModel> {
 
   constructor(rootElement: JQuery, model: IModel) {
-
     super(new views.View(rootElement), model);
+  }
 
+  private exampleButtonClickedAction() {
+    var data = this.view.read();
+    this.view.bind({ message: 'Hello ' + data.name + '!' });
   }
 
 }
